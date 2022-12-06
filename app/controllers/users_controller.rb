f15e154fdd3d3a76b6ab51189cd3da8ccd8cbbd0
geogrey12muiruri <greggambugua@gmail.com>
2022-12-06 12:20:42 +0300

    BF-8 <mmodified the user password to password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
     private
     # Only allow a trusted parameter "white list" through.
     def user_params
-        params.require(:user).permit(:name, :email :username, :password, :password_confirmation)
+        params.require(:user).permit(:name, :email :username, :password_digest, :password_confirmation)
     end
 end
