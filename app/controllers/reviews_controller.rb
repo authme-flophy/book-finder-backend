76038130c7e4cff0bd1d1e65154bd075c1775567
geogrey12muiruri <greggambugua@gmail.com>
2022-12-06 11:18:33 +0300

    BF-8 added reviews controllers crud operations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,27 @@
 class ReviewsController < ApplicationController
+    def index
+        @reviews = Review.all
+    end
+   
+    def show
+        @reviews = Review.find(params[:id])
+    end
     
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            render json: @review
+        else
+            render json: @review.errors, status: :unprocessable_entity
+        end
+    end
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+    end
+    private
+    def review_params
+        params.require(:review).permit(:book_id, :user_id, :comment)
+    end
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_06_074822) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
