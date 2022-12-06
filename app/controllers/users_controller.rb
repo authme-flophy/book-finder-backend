d4a084164f84dd6007eb330047584335bb7b67da
geogrey12muiruri <greggambugua@gmail.com>
2022-12-06 10:14:25 +0300

    added user controller's crud operations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,35 @@
 class UsersController < ApplicationController
+    #create users and validate params
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+   #update users and validate params
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+    #delete users
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+    end
+    private
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+        params.require(:user).permit(:name, :email :username, :password, :password_confirmation)
+    end
 end
