5ddd28defbf842f41984c75c98f9fc813366e9e4
Peter <peterkilungya1@gmail.com>
2022-12-07 11:46:36 +0300

    done with serializers

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,13 @@ module BookFinderBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
+    enerating a new resource.
+    config.api_only = true
+  end
+end # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
@@ -33,7 +38,4 @@ module BookFinderBackend
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-  end
-end
+    # Skip views, helpers and asse
