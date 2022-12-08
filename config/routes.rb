be5a23df6cb1caa9510ef31ef3f83f09ffa3af52
waikau maina <ericmaina.dev@gmail.com>
2022-12-08 16:56:45 +0300

    edited the routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  post "/login", to: "session#create"
-  delete "/logout", to: "session#destroy"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   get "/me", to: "users#show"
   post "/signup", to: "users#create"
 
