b1664884834916293e6692ca296338a52e1c1590
waikau maina <ericmaina.dev@gmail.com>
2022-12-08 17:01:12 +0300

    edited the books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,42 +1,19 @@
 class BooksController < ApplicationController
-    def index
-        @books = Book.all
-        #search for books by title
-        @books = @books.where("title LIKE ?", "%#{params[:title]}%") if params[:title].present?
-        render json: @books
-    end
-    def create
-        @book = Book.new(book_params)
-        if @book.save
-            render json: @book
-        else
-            render json: @book.errors, status: :unprocessable_entity
-        end
-    end
-  #display books and authors and reviews
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def index
+    books = Book.all
+    render json: books
+  end
+
   def show
-    @book = Book.find(params[:id])
-    render json: @book
-    @reviews = Review.where(book_id: @book.id)
-    @book_category = BookCategory.where(book_id: @book.id)
-  end
-
-  def update
-    @book = Book.find(params[:id])
-    if @book.update(book_params)
-      render json: @book
-    else
-      render json: @book.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
+    book = Book.find(params[:id])
+    render json: book
   end
 
   private
-  def book_params
-    params.require(:book).permit(:title, :author, :likes, :category_id)
+
+  def render_not_found_response
+    render json: { error: "Validation errors" }, status: :not_found
   end
 end
