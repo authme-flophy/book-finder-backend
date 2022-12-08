6542c0ea50d6024a079c232a25b369f863978e28
waikau maina <ericmaina.dev@gmail.com>
2022-12-08 17:00:35 +0300

    edited the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,28 +1,31 @@
 class ReviewsController < ApplicationController
-    def index
-        reviews = Review.all
-        render json: reviews
-    end
-   
-    def show
-        @reviews = Review.find(params[:id])
-    end
-    
-    def create
-        @review = Review.new(review_params)
-        if @review.save
-            render json: @review
-        else
-            render json: @review.errors, status: :unprocessable_entity
-        end
-    end
-    def destroy
-        @review = Review.find(params[:id])
-        @review.destroy
-    end
-    private
-    def review_params
-        params.require(:review).permit(:book_id, :user_id, :comment)
-    end
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+  def index
+    reviews = Review.all
+    render json: reviews
+  end
+ 
+  def show
+    review = Review.find(params[:id])
+    render json: review
+  end
+  
+  def create
+      review = Review.create!(book_id: params[:book_id], user_id: session[:user_id], comment: params[:comment])
+      render json: review
+  end
+
+  def destroy
+      @review = Review.find(params[:id])
+      @review.destroy
+  end
+
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Validation errors" }, status: :not_found
+  end
    
 end
