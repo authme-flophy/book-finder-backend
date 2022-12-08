21ea2be91c55fbc0f2fc717679c988f1a8474186
waikau maina <ericmaina.dev@gmail.com>
2022-12-08 16:55:46 +0300

    edited the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :bookcategories
-  resources :books
-  resources :reviews, only: [:index]
-  resources :users,  only: [:index]
-  post "/signin", to: "sessions#create_session"
-  post "/signup", to: "users#create"
-  #get "signout" => "sessions#destroy"
-  
-  
+  resources :bookcategories, only: [:index, :show]
+  resources :books, only: [:index, :show]
+  resources :reviews, only: [:index, :show, :create, :destroy, :update]
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+
   # root "articles#index"
 end
